069b8aea1188b26fa06565db0c20302d67ed3eb1
WildHenn <j.kalunseviko@gmail.com>
2025-02-24 14:47:41 +0100

    sample of 800 articles to test classifier

--- filter_sample.py
+++ filter_sample.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Load your filtered dataset
-df = pd.read_csv("filtered_articles_expanded.csv", encoding="utf-8")
+df = pd.read_csv("data/filtered_articles_expanded.csv", encoding="utf-8")
 
-# Sample 100 random articles
-df_sample = df.sample(n=100)
+# Sample 800 random articles
+df_sample = df.sample(n=800)
 
 # Save the sample dataset to a new CSV file for categorization in Label Studio
-df_sample.to_csv("sample_for_labeling.csv", encoding="utf-8", index=False)
+df_sample.to_csv("sample_for_labeling_2.csv", encoding="utf-8", index=False)
 
-print(f"100 sample articles have been saved to 'sample_for_labeling.csv'with {len(df)} articles.")
+print(f"800 sample articles have been saved to 'sample_for_labeling_2.csv'with {len(df)} articles.")
